1822f7b21e2d04dcd6686be716ae7d564dd57b35
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-09 17:04:47 -0400

    Interacting with db with sample data

---
+++ meetup-api/src/main/java/com/meetup/meetupapi/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.meetup.meetupapi.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+class UserController{
+    
+    @GetMapping(path="/")
+    public String index(){
+        return "Congrats, it works";
+    }
+
+    @GetMapping(path="/login")
+    public String login(){
+        return "Now you're at the login";
+    }
+}
