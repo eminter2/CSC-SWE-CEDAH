568aca673e74213ef1877d61a8ff7c30934494be
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-09 17:04:05 -0400

    Deleted

--- meetup-api/src/main/java/com/meetup/meetupapi/UserRepository.java
+++
@@ -1,7 +0,0 @@
-// package com.meetup.meetupapi;
-// import java.util.Optional;
-// import org.springframework.data.jpa.repository;
-
-// public interface UserRepository extends JpaRepository<User, Integer>{
-//     Optional<User> findByUsername(String username);
-// }
