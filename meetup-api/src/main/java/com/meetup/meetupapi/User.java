922628f01e577a6ef458733c509bfe4151bc4d12
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-09 16:01:23 -0400

    Connected to a local sqlserver without breaking

--- meetup-api/src/main/java/com/meetup/meetupapi/User.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/User.java
@@ -1,45 +1,45 @@
-package com.meetup.meetupapi;
+// package com.meetup.meetupapi;
 
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import java.util.HashSet;
-import java.util.Set;
+// import javax.persistence.*;
+// import javax.validation.constraints.Email;
+// import javax.validation.constraints.NotBlank;
+// import javax.validation.constraints.Size;
+// import java.util.HashSet;
+// import java.util.Set;
 
-@Entity
-@Table(name = "login", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {
-            "username"
-        }),
-        @UniqueConstraint(columnNames = {
-            "password"
-        })
-})
-public class User {
-    private String username;
-    private String password;
+// @Entity
+// @Table(name = "login", uniqueConstraints = {
+//         @UniqueConstraint(columnNames = {
+//             "username"
+//         }),
+//         @UniqueConstraint(columnNames = {
+//             "password"
+//         })
+// })
+// public class User {
+//     private String username;
+//     private String password;
 
-    // public int getId(){
-    //     return id;
-    // }
-    // public void setId(int id){
-    //     this.id = id;
-    // }
+//     // public int getId(){
+//     //     return id;
+//     // }
+//     // public void setId(int id){
+//     //     this.id = id;
+//     // }
 
-    public String getUsername(){
-        return username;
-    }
+//     public String getUsername(){
+//         return username;
+//     }
 
-    public void setUsername(String username){
-        this.username = username;
-    }
+//     public void setUsername(String username){
+//         this.username = username;
+//     }
 
-    public String getPassword(){
-        return password;
-    }
+//     public String getPassword(){
+//         return password;
+//     }
 
-    public void setPassword(String password){
-        this.password = password;
-    }
-}
+//     public void setPassword(String password){
+//         this.password = password;
+//     }
+// }
--- meetup-api/src/main/java/com/meetup/meetupapi/UserController.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/UserController.java
@@ -1,30 +1,17 @@
 package com.meetup.meetupapi;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Collection;
-import java.util.Optional;
-
-
-
 @RestController
-@RequestMapping("/api")
 class UserController{
-
-    private UserRepository userRepository;
-
-    public UserController(UserRepository userRepository){
-        this.userRepository = userRepository;
+    
+    @GetMapping(path="/")
+    public String index(){
+        return "Congrats, it works";
     }
 
-    @GetMapping(path="/login/{username}")
-    ResponseEntity<?> getUser(@PathVariable String username){
-        Optional<User> user = userRepository.findByUsername(username);
-        return user.map(response -> ResponseEntity.ok().body(response)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    @GetMapping(path="/login")
+    public String login(){
+        return "Now you're at the login";
     }
 }
