cc0a88c449e022645ed002a7f0baff51e0df7ff9
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-29 17:54:46 -0400

    Adding dashaboard component from other branch.

--- app/src/components/Dashboard.js
+++ app/src/components/Dashboard.js
@@ -1,9 +1,38 @@
-import React from 'react';
+ 
+import React, {useEffect, useState} from 'react';
+import './Dashboard.css';
+import Header from './Header';
 
-const Dashboard = () => {
+const Dashboard = (props) => {
+
+    const [username, setUsername] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        gatherData();
+    }, [])
+
+    const gatherData = async () => {
+        // let {username, email} = await getUser()
+        // setUsername(username)
+        // setLoading(true)
+        // await getUserGroups(csrfToken, email)
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000);
+    }
+
+    const logout = () => {
+        
+      }
+
     return (
-        <div>You're at the dashboard</div>
+        <div className="dashboard">
+            <Header condensed={true} logout={logout} isAuthenticated={true} />
+            <h1>Welcome to your Dashboard, {username}!</h1>
+            {loading ? <p style={{textAlign: 'center'}}>loading...</p> : <p style={{textAlign: 'center'}}>here's ur content ideot</p>}
+        </div>
     )
 }
 
-export default Dashboard
+export default Dashboard;
