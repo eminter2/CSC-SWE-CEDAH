d1682f97f6a513cea322fb20a3dd54b4524f853a
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-29 21:19:19 -0400

    deleted unused files, added styling

--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
+import {Jumbotron, Button} from 'react-bootstrap';
 import './Welcome.css';
 import Header from './Header';
 
@@ -10,6 +11,14 @@ const Welcome = () => {
           <Header />:
           <Header isAuthenticated={true}/>
         }
+        <Jumbotron>
+          <h1>Welcome to MeetUP!</h1>
+          <p>
+            This is our way of simplifying collaboration.
+            Let AI do the work. We want to give you your time back.
+          </p>
+            <Button variant="primary" size="lg">Learn more</Button>
+        </Jumbotron>
       </div>
     );
 }
