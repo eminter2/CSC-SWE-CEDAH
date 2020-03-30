6dafbb65bb1fdc931f180f25375529ac4546eb16
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-29 21:06:29 -0400

    removed unnecessary code

--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -1,29 +1,17 @@
-import React, {useState} from 'react';
-import {Redirect, withRouter} from 'react-router-dom';
+import React from 'react';
+import {withRouter} from 'react-router-dom';
 import './Welcome.css';
 import Header from './Header';
 
 const Welcome = () => {
-  const [redirect, setRedirect] = useState(false);
-
-  if(localStorage.token){
-    return <Redirect push to='/dashboard'/>
-  }
-
-  function handleLogin(){
-    setRedirect(true)
-  }
-
-  if(redirect){
-    return <Redirect push to='/login'/>
-  }
-  else{
     return (
       <div className="welcome">
-        <Header login={handleLogin}/>
+        {!localStorage.token ? 
+          <Header />:
+          <Header isAuthenticated={true}/>
+        }
       </div>
     );
-  }
 }
 
 export default withRouter(Welcome);
