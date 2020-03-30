ab7b890a997f1d5f003d87681e8a4045bcb1e308
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-29 20:50:09 -0400

    enabled redirects and login after registration. need to polish later

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {Form, Button} from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Form, Button, Spinner} from 'react-bootstrap';
 import Header from './Header';
 import {withRouter, Redirect} from 'react-router-dom';
 
@@ -8,14 +8,17 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
     const [checked, setChecked] = useState(false);
+    const [isLoading, setLoading] = useState(false);
+    const [redirect, setRedirect] = useState(false);
 
     let formUser = {
-    username: username,
-    password: password
+        username: username,
+        password: password
     }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        setLoading(true);
 
         //Login existing user
         if(!checked){
@@ -32,25 +35,28 @@ const Login = () => {
                 body: JSON.stringify(formUser)
             })
             .then((response) => {
-                // console.log('Response', response.headers.get("Authorization")) 
-                let token = response.headers.get("Authorization").split("Bearer ")[1]
-                // console.log(token)
-                localStorage.setItem("token", token)    
-                if(!response.ok){
+                let authHeader = response.headers.get("Authorization")
+                setLoading(false)
+                if(authHeader === null){
                     setMessage("Username or Password are incorrect")
                     setTimeout(() => {
                         setMessage("")
                     }, 3000);
                 }
+                else{
+                    let token = authHeader.split("Bearer ")[1]
+                    localStorage.setItem("token", token)
+                    setRedirect(true)    
+                } 
             })
             .catch((error) => {
                 console.log('Request Failed: ', error)
             })
         }
+
         //Register new user
         else {
             let url = '/users/sign-up'
-            console.log(`Attempting registration for ${formUser.username}, password ${formUser.password}`)
             fetch(url, {
                 method: 'POST',
                 headers: {
@@ -60,8 +66,38 @@ const Login = () => {
                 body: JSON.stringify(formUser)
             })
             .then(response => {
-                let data = response.json()
-                setMessage(`Account Created for ${data}`)
+                //Registered user, post login now
+                if(response.ok){
+                    console.log('Account created...logging in...')
+                    fetch('/login' , {
+                        method: 'POST',
+                        cache: 'no-cache',
+                        credentials: 'same-origin',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(formUser)
+                    })
+                    .then(response => {
+                        let authHeader = response.headers.get("Authorization")
+                        setLoading(false)
+                        if(authHeader === null){
+                            setMessage("Username or Password are invalid")
+                            setTimeout(() => {
+                                setMessage("")
+                            }, 3000);
+                        }
+                        else{
+                            let token = authHeader.split("Bearer ")[1]
+                            localStorage.setItem("token", token)
+                            setRedirect(true)    
+                        } 
+                    })
+                    .catch(err => {
+                        console.log('Something went wrong', err)
+                    })
+                }
             })
             .catch((error) => {
                 console.log("Error from server: ", error)
@@ -70,46 +106,63 @@ const Login = () => {
 
     }
 
-    return (
-        <div>
-            <Header/>
-            <h1>Login / SignUP</h1>
-            <div className="login-form">
-              <Form onSubmit={handleSubmit}>
-                <Form.Label 
-                  style={
-                    { color: 'red', 
-                      display: 'block', 
-                      textAlign: 'center'}
-                  }>{message}</Form.Label>
-                <Form.Group controlId="formGroupUsername">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control 
-                    type="text" 
-                    placeholder="Enter username" 
-                    value={username}
-                    onChange={e => setUsername(e.target.value)}
-                    autoFocus={true}/>
-                </Form.Group>
-      
-                <Form.Group controlId="formGroupPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control 
-                    type="password" 
-                    placeholder="Password" 
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}/>
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="I'm a New User" onChange={e => setChecked(!checked)}/>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button> 
-              </Form>
+    if(redirect) return <Redirect push to='/dashboard'/>
+
+    else {
+        return (
+            <div>
+                <Header/>
+                <h1>Login / SignUP</h1>
+                <div className="login-form">
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Label 
+                        style={
+                            { color: 'red', 
+                            display: 'block', 
+                            textAlign: 'center'}
+                        }>{message}</Form.Label>
+                        <Form.Group controlId="formGroupUsername">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                placeholder="Enter username" 
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                                autoFocus={true}/>
+                        </Form.Group>
+            
+                        <Form.Group controlId="formGroupPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control 
+                                type="password" 
+                                placeholder="Password" 
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}/>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="I'm a New User" onChange={e => setChecked(!checked)}/>
+                        </Form.Group>
+                        {isLoading ?
+                            <Button variant="primary" type="submit"> 
+                                <Spinner
+                                        as="span"
+                                        animation="border"
+                                        size="sm"
+                                        role="status"
+                                        aria-hidden="true"
+                                        />
+                                Loading...
+                                <span className="sr-only">Loading...</span>
+                            </Button>
+                            :
+                            <Button variant="primary" type="submit">Submit</Button>
+                        }
+                    </Form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default withRouter(Login);
