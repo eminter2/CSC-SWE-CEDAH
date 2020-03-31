4831c9f5f221c0945a70fe7d1ad760017e49fe77
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-31 18:38:28 -0400

    Removed imports for unlinked deps

--- app/src/components/Meeting.js
+++ app/src/components/Meeting.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Card, Button} from 'react-bootstrap';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCalendar} from '@fortawesome/free-solid-svg-icons';
 
 const Meeting = (props) => {
 
