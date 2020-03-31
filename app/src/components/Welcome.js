8b45862a60ae029604e3da9c92c8848dcb5087c9
ConnorLacy <lacyconnor1409@gmail.com>
2020-03-31 16:09:45 -0400

    Sticky header as fragment

--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -6,46 +6,50 @@ import Header from './Header';
 
 const Welcome = () => {
     return (
-      <div className="page welcome">
-        {!localStorage.token ? 
-          <Header />:
-          <Header isAuthenticated={true}/>
-        }
+      <>
+        <>
+          {!localStorage.token ? 
+            <Header />:
+            <Header isAuthenticated={true}/>
+          }
+        </>
+        <div className="page welcome">
 
-        <section className="section 1" href="#about" >
-          <h1>About things</h1>
-        </section>
-        <section className="section 2" href="#about">
-          <h1>About things</h1>
-        </section>
-        <section className="section 3" href="#">
-          <h1>About things</h1>
-        </section>
+          <section className="section 1" href="#about" >
+            <h1>About things</h1>
+          </section>
+          <section className="section 2" href="#about">
+            <h1>About things</h1>
+          </section>
+          <section className="section 3" href="#">
+            <h1>About things</h1>
+          </section>
 
 
-        <Container fluid className="footer">
-          <Row md={4}>
-            <Col>
-              <h1>Branding</h1>
-            </Col>
-            <Col>
-              <p>Something</p>
-              <p>Something2</p>
-              <p>Something3</p>
-            </Col>
-            <Col>
-              <p>Something</p>
-              <p>Something</p>
-              <p>Something</p>
-            </Col>
-            <Col>
-              <p>Something</p>
-              <p>Something</p>
-              <p>Something</p>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+          <Container fluid className="footer">
+            <Row md={4}>
+              <Col>
+                <h1>Branding</h1>
+              </Col>
+              <Col>
+                <p>Something</p>
+                <p>Something2</p>
+                <p>Something3</p>
+              </Col>
+              <Col>
+                <p>Something</p>
+                <p>Something</p>
+                <p>Something</p>
+              </Col>
+              <Col>
+                <p>Something</p>
+                <p>Something</p>
+                <p>Something</p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </>
     );
 }
 
