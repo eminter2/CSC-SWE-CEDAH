bf11891c2cebf13ede058330d06b51b317f7a73b
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-04 20:48:56 -0400

    separated logic for login and sign up

--- app/src/components/Signup.js
+++ app/src/components/Signup.js
@@ -1,18 +1,60 @@
-import React from 'react';
-import {withRouter} from 'react-router-dom';
+import React, {useState} from 'react';
+import {withRouter, NavLink, Redirect} from 'react-router-dom';
 import Userform from './Userform';
 
 import './Signup.css';
 
 const Signup = () => {
 
-    return (
-        <div className="page signup">
-            <h1>Sign up!</h1>
-            <Userform signup={true}/>
-        </div>
-    )
+    const [isLoading, setLoading] = useState(false);
+    const [message, setMessage] = useState("");
+    const [redirect, setRedirect] = useState(false);
 
+    const handleSubmit = async (e, formData) => {
+        e.preventDefault()
+        setLoading(true);
+        console.log("Form data: ", formData)
+
+        //Register new user
+        let url = '/users/sign-up'
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        if(response.ok){
+            setRedirect(true)
+        }
+        else{
+            let responseText = await response.text()
+            console.log('Response text: ', responseText)
+            setMessage(responseText);
+        }
+        setLoading(false)
+    }
+
+    if(redirect) return <Redirect to="/login" exact push/>
+    else{
+        return (
+            <div className="page signup">
+                <h1>Sign up!</h1>
+                <div className="login-form">
+                    <p style={{color: 'red'}}>{message}</p>
+                    <Userform 
+                        signup
+                        isLoading={isLoading}
+                        handleSubmit={handleSubmit}/>
+                    <p style={{padding: 30}}>
+                        Already have an account?
+                        <NavLink to="/login" style={{marginLeft: 10}}>Log in</NavLink>
+                    </p>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default withRouter(Signup);
--- app/src/components/Userform.js
+++ app/src/components/Userform.js
@@ -2,120 +2,178 @@ import React, {useState} from 'react';
 import {Form, Button, Spinner} from 'react-bootstrap';
 
 const Userform = (props) => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [newUser, setnewUser] = useState(false);
+    const [password2, setPassword2] = useState("");
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
 
-    let formUser = {
+
+    let formData = {
+        fullName: fullName,
+        email: email,
+        phone: phone,
         username: username,
         password: password,
     }
 
+    const validateAndSubmit = (e) => {
+        setLoading(true)
+        e.preventDefault();
+        if(password !== password2){
+            setMessage("Passwords do not match")
+        }
+        else {
+            props.handleSubmit(e, formData)
+        }
+    }
+
     if(props.signup){
         return (
-            <div className="login-form">
-                <Form 
-                    style={{
-                        width: '30%',
-                        maxWidth: 500,
-                        minWidth: 250
-                    }}
-                    onSubmit={e => props.handleSubmit(e, formUser, newUser)}>
-                    <Form.Label 
+            <Form 
+                style={{
+                    width: '30%',
+                    maxWidth: 500,
+                    minWidth: 250
+                }}
+                onSubmit={e => validateAndSubmit(e)}>
+                <Form.Label 
+                style={
+                    { color: 'red', 
+                    display: 'block', 
+                    textAlign: 'center'}
+                }>{props.message}</Form.Label>
+                <Form.Group controlId="fname">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        value={fullName}
+                        onChange={e => setFullName(e.target.value)}
+                        autoFocus={true}
+                        required/>
+                </Form.Group>
+                <Form.Group controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control 
+                        type="email" 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        required
+                        />
+                </Form.Group>
+                <Form.Group controlId="phone">
+                    <Form.Label>Phone</Form.Label>
+                    <Form.Control 
+                        type="phone" 
+                        value={phone}
+                        onChange={e => setPhone(e.target.value)}
+                        required
+                        />
+                </Form.Group>
+                <Form.Group controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        required
+                        />
+                </Form.Group>
+                <Form.Label 
                     style={
                         { color: 'red', 
                         display: 'block', 
                         textAlign: 'center'}
-                    }>{props.message}</Form.Label>
-                    <Form.Group controlId="formGroupUsername">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Enter username" 
-                            value={username}
-                            onChange={e => setUsername(e.target.value)}
-                            autoFocus={true}/>
-                    </Form.Group>
-                    <Form.Group controlId="formGroupPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control 
-                            type="password" 
-                            placeholder="Password" 
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}/>
-                    </Form.Group>
-                    {props.isLoading ?
-                        <Button variant="primary" type="submit"> 
-                            <Spinner
-                                    as="span"
-                                    animation="border"
-                                    size="sm"
-                                    role="status"
-                                    aria-hidden="true"
-                                    />
-                            Loading...
-                            <span className="sr-only">Loading...</span>
-                        </Button>
-                        :
-                        <Button variant="primary" type="submit">Submit</Button>
-                    }
-                </Form>
-            </div>
+                    }>{message}</Form.Label>
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        value={password}
+                        onChange={e => {setPassword(e.target.value); setMessage("")}}
+                        required
+                        />
+                </Form.Group>
+                <Form.Group controlId="password2">
+                    <Form.Label>Re-enter Password</Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        value={password2}
+                        onChange={e => {setPassword2(e.target.value); setMessage("")}}
+                        required
+                        />
+                </Form.Group>
+                {props.isLoading ?
+                    <Button variant="primary" type="submit"> 
+                        <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                                />
+                        Loading...
+                        <span className="sr-only">Loading...</span>
+                    </Button>
+                    :
+                    <Button variant="primary" type="submit">Submit</Button>
+                }
+            </Form>
         )
     }
     else {
         return (
-            <div className="login-form">
-                <Form 
-                    style={{
-                        width: '30%',
-                        maxWidth: 500,
-                        minWidth: 250
-                    }}
-                    onSubmit={e => props.handleSubmit(e, formUser, newUser)}>
-                    <Form.Label 
-                    style={
-                        { color: 'red', 
-                        display: 'block', 
-                        textAlign: 'center'}
-                    }>{props.message}</Form.Label>
-                    <Form.Group controlId="formGroupUsername">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Enter username" 
-                            value={username}
-                            onChange={e => setUsername(e.target.value)}
-                            autoFocus={true}/>
-                    </Form.Group>
-                    <Form.Group controlId="formGroupPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control 
-                            type="password" 
-                            placeholder="Password" 
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="I'm a New User" onChange={e => setnewUser(!newUser)}/>
-                    </Form.Group>
-                    {props.isLoading ?
-                        <Button variant="primary" type="submit"> 
-                            <Spinner
-                                    as="span"
-                                    animation="border"
-                                    size="sm"
-                                    role="status"
-                                    aria-hidden="true"
-                                    />
-                            Loading...
-                            <span className="sr-only">Loading...</span>
-                        </Button>
-                        :
-                        <Button variant="primary" type="submit">Submit</Button>
-                    }
-                </Form>
-            </div>
+            <Form 
+                style={{
+                    width: '30%',
+                    maxWidth: 500,
+                    minWidth: 250
+                }}
+                onSubmit={e => props.handleSubmit(e, formData)}>
+                <Form.Label 
+                style={
+                    { color: 'red', 
+                    display: 'block', 
+                    textAlign: 'center'}
+                }>{props.message}</Form.Label>
+                <Form.Group controlId="formGroupUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Enter username" 
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        autoFocus={true}
+                        required/>
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password" 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required/>
+                </Form.Group>
+                {props.isLoading ?
+                    <Button variant="primary" type="submit"> 
+                        <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                                />
+                        Loading...
+                        <span className="sr-only">Loading...</span>
+                    </Button>
+                    :
+                    <Button variant="primary" type="submit">Submit</Button>
+                }
+            </Form>
         )
     }
 }
