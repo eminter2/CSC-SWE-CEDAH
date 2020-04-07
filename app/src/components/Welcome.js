cf96ef6c88e1b984916869381c643a43e26d4cfe
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-06 22:32:26 -0400

    init sass & welcome page style

--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
-import './Welcome.css';
 
 const Welcome = () => {
     return (
--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import configureStore from './redux/store';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
