fdc7bfd3a1e1632a7766eb5bd257053e2c64ed70
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-16 15:57:40 -0400

    init of add group component

---
+++ app/src/pages/AddGroup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddGroup = () => {
+    return (
+        <div className="page add-group">
+            <h1 className="fathead">Add a Group</h1>
+            <div className="form add-group"></div>
+        </div>
+    )
+}
+
+export default AddGroup;
