19f4411af6d680f028ccc593bb2c460eac980535
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-16 19:37:07 -0400

    reformatted and added link to group view component

--- app/src/components/Group.js
+++ app/src/components/Group.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import {Button, Card} from 'react-bootstrap';
+import {NavLink} from 'react-router-dom';
 
 const Group = (props) => {
+    console.log(props)
     return (
         <Card style={{marginBottom: 15}}>
-            <Card.Header as="h5">Group {props.group.GroupNumber}</Card.Header>
+            <Card.Header as="h5">
+                <strong>Group</strong> {props.group.GroupNumber}<br/>
+                <strong>Owner</strong> {props.group.Owner}
+                </Card.Header>
             <Card.Body>
-                <Card.Title>Group Name</Card.Title>
                 <Card.Text>
                 With supporting text below as a natural lead-in to additional content.
-                <br/>Owner: {props.group.Owner}
                 </Card.Text>
-                <Button variant="primary">Open Group</Button>
+                <Button 
+                    as={NavLink}
+                    to={`/group/view/${props.group.GroupNumber}`}
+                    variant="primary" 
+                    >
+                        Open Group
+                </Button>
             </Card.Body>
         </Card>
     )
