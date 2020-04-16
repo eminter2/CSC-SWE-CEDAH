25304c558377ca788be76f7f556af79f0802824e
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-16 17:55:19 -0400

    added request mapping for group actions

--- meetup-api/src/main/java/com/meetup/meetupapi/controller/MeetupGroupController.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/controller/MeetupGroupController.java
@@ -5,6 +5,7 @@ import com.meetup.meetupapi.repo.MeetupGroupRepository;
 import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +20,19 @@ public class MeetupGroupController {
 
     public MeetupGroupController(MeetupGroupRepository meetupGroupRepository){
         this.meetupGroupRepository = meetupGroupRepository;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addGroup(@RequestParam("id") int userId){
+        JSONObject response = new JSONObject();
+        response.put("data", "success");
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<?> joinGroup(@RequestParam("id") int userId){
+        JSONObject response = new JSONObject();
+        response.put("data", "success");
+        return ResponseEntity.status(200).body(response);
     }
 }
