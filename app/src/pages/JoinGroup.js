33ce3bb2fad4802f9f93afdbb2a9addea52cc915
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-16 15:57:49 -0400

    init join group component

---
+++ app/src/pages/JoinGroup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JoinGroup = () => {
+    return (
+        <div className="page join-group">
+            <h1 className="fathead">Join a Group</h1>
+        </div>
+    )
+}
+
+export default JoinGroup;
