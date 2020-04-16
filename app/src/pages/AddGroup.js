773ebca997df8aa38bb7f3ab0c316a99612017c3
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-16 16:30:14 -0400

    created new header class

--- app/src/pages/AddGroup.js
+++ app/src/pages/AddGroup.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AddGroup = () => {
     return (
         <div className="page add-group">
-            <h1 className="fathead">Add a Group</h1>
+            <h1 className="huskyhead">Add a Group</h1>
             <div className="form add-group"></div>
         </div>
     )
--- app/src/pages/JoinGroup.js
+++ app/src/pages/JoinGroup.js
@@ -3,7 +3,7 @@ import React from 'react';
 const JoinGroup = () => {
     return (
         <div className="page join-group">
-            <h1 className="fathead">Join a Group</h1>
+            <h1 className="huskyhead">Join a Group</h1>
         </div>
     )
 }
