86fcde75e294fcce046fc9dec1b91f6ffd58ef4f
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-16 18:02:06 -0400

    added join group front end triggers

--- app/src/redux/actions/groups.js
+++ app/src/redux/actions/groups.js
@@ -19,3 +19,24 @@ export const addGroup = (userId, token) => {
     }
 }
 
+export const joinGroup = (userId, token) => {
+    console.log('made it to joinGroup')
+    return dispatch => {
+        return fetch(`/group/join?id=${userId}`, {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Response: ', data.data)
+            // dispatch(addGroup(data.group))
+        })
+        .catch(err => console.log('Error: ', err))
+    }
+}
+
