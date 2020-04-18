5c3639aa521963df091f563d2d2d54d679510013
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-18 18:44:35 -0400

    added new mapaping for retrieving groups

--- meetup-api/src/main/java/com/meetup/meetupapi/controller/MeetupGroupController.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/controller/MeetupGroupController.java
@@ -1,25 +1,42 @@
 package com.meetup.meetupapi.controller;
 
+import java.util.List;
+
+import com.meetup.meetupapi.model.MeetupGroup;
 import com.meetup.meetupapi.repo.MeetupGroupRepository;
 
 import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/group")
+@RequestMapping("/groups")
 public class MeetupGroupController {
 
     private MeetupGroupRepository meetupGroupRepository;
 
     public MeetupGroupController(MeetupGroupRepository meetupGroupRepository){
         this.meetupGroupRepository = meetupGroupRepository;
+    }
+
+    @PostMapping("/retrieve")
+    public ResponseEntity<?> getGroups(@RequestParam("id") int userId){
+        JSONObject response = new JSONObject();
+        int status = 200;
+        List<MeetupGroup> userGroups;
+        try {
+            userGroups = meetupGroupRepository.findMyGroups(userId);
+            response.put("groups", userGroups);
+        } catch (Exception e){
+            System.out.println("Query failed");
+            status = 500;
+            response.put("message", "Query Failed");
+        }
+
+        return ResponseEntity.status(status).body(response);
     }
 
     @PostMapping("/add")
