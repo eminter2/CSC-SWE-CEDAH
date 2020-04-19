6283c188bb078ee90611b4972911cebbfff043de
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-18 23:57:40 -0400

    cleanup

--- meetup-api/src/main/java/com/meetup/meetupapi/repo/ApplicationUserRepository.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/repo/ApplicationUserRepository.java
@@ -3,7 +3,6 @@ package com.meetup.meetupapi.repo;
 import com.meetup.meetupapi.model.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
     ApplicationUser findByUsername(String username);
     ApplicationUser findByEmail(String email);
