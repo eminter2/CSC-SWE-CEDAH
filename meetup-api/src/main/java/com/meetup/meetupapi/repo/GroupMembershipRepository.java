bb29ba354fdc048be9ee97789b499b4d9a9ad2fd
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-19 06:14:00 -0400

    leave group custom query

--- meetup-api/src/main/java/com/meetup/meetupapi/repo/GroupMembershipRepository.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/repo/GroupMembershipRepository.java
@@ -28,4 +28,16 @@ public interface GroupMembershipRepository extends JpaRepository<GroupMembership
         nativeQuery = true
     )
     int joinGroup(int userId, String groupName);
+
+
+    @Transactional
+    @Modifying
+    @Query(
+        value = "DELETE FROM group_membership WHERE user_id = ?1 AND group_id = ?2 ",
+        nativeQuery = true
+    )
+    int deleteFromGroup(int userId, int groupId);
+
+
+    
 }
