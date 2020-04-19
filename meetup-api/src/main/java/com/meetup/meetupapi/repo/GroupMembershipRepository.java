2278480bd4677db4c204583d5043b841c0a8a710
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-18 23:57:59 -0400

    added custom query

--- meetup-api/src/main/java/com/meetup/meetupapi/repo/GroupMembershipRepository.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/repo/GroupMembershipRepository.java
@@ -1,8 +1,17 @@
 package com.meetup.meetupapi.repo;
 
+import java.util.List;
+
 import com.meetup.meetupapi.model.GroupMembership;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 
 public interface GroupMembershipRepository extends JpaRepository<GroupMembership, Long> {
+    @Query(
+        value = "SELECT * " +
+        "FROM group_membership AS M, users AS U " +
+        "WHERE M.user_id = U.id AND M.group_id = ?1",
+        nativeQuery = true
+    )
+    List<GroupMembership> findMembers(int groupId);
 }
